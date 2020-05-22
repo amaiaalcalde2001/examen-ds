4c49d7d669157425b08a883d08cbb43822545a1b
Amaia <amaia.alcalde@alumni.mondragon.edu>
2020-05-22 10:59:45 +0200

    genero

--- Carga fichero.R
+++ Carga fichero.R
@@ -2,3 +2,11 @@ load("C:/Users/amaia/OneDrive/Escritorio/1 BDATA/DATA SCIENCE/EXAMEN_DS/Archivos
 dim(muestra02)
 #el fichero tien dimensiones de 297111 filas y 3 columnas
 colnames(muestra02)<- c("nif", "nombre", "genero")
+unos<-c()  
+genero$genero_imp[genero$n_M>genero$n_V]="M"
+genero$genero_imp[genero$n_V>genero$n_M]="V"
+
+muestra02<-muestra02%>%
+  group_by(genero)%>%
+  group_by(nombre)%>%
+  mutate(unos<-n())
