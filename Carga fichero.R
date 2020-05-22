87f061173cc04ecae49201deece153e9b41de5d9
Amaia <amaia.alcalde@alumni.mondragon.edu>
2020-05-22 10:01:50 +0200

    cambiamos nombres columnas

--- Carga fichero.R
+++ Carga fichero.R
@@ -1,3 +1,4 @@
 load("C:/Users/amaia/OneDrive/Escritorio/1 BDATA/DATA SCIENCE/EXAMEN_DS/Archivos_examen_ds/muestra02.RData")
 dim(muestra02)
 #el fichero tien dimensiones de 297111 filas y 3 columnas
+colnames(muestra02)<- c("nif", "nombre", "genero")
