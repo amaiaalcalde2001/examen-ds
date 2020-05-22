1a5c24a0a96c8d73670fe4855c1f5eb4862e9156
Amaia <amaia.alcalde@alumni.mondragon.edu>
2020-05-22 09:58:55 +0200

    dimensiones añadidas

---
+++ Carga fichero.R
@@ -0,0 +1,3 @@
+load("C:/Users/amaia/OneDrive/Escritorio/1 BDATA/DATA SCIENCE/EXAMEN_DS/Archivos_examen_ds/muestra02.RData")
+dim(muestra02)
+#el fichero tien dimensiones de 297111 filas y 3 columnas
